86f048d7cd7aec28687c4b1de3b95fb4977721db
Saif Khan <47074353+SaifKhan09@users.noreply.github.com>
2024-05-23 02:12:36 +0530

    Update pattern-18.py

--- Patterns/pattern-18.py
+++ Patterns/pattern-18.py
@@ -18,19 +18,3 @@ printPattern(5)
 # *       *
 # * * * * *
 
-# i < n-1 star = n/2 , space = 2*(n-i-1) , star = i+1
-# i = n-1 star = n , space = n-i-1 , start = n
-# i > n-1 star = i-n + 1 , space = 2*(n-1)+1 - i , start = i -n +1
-
-
-# n = 5
-# row(i) star space star 
-# 0       2    0     0 
-# 1       1    2     1
-# 2       1    2     1
-# 3       2    0     2
-# 4       5    0     5   
-# 5       4    2     4         
-# 6       3    4     3
-# 7       2    6     2 
-# 8       1    8     1 
